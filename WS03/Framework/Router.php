8f49ca69a74666e06f53737d7120011bf68b5317
Lan Angeles <lanargied.angeles2004@gmail.com>
2026-05-19 14:15:41 +0800

    Login and Authorize Middleware 2/2

--- WS03/Framework/Router.php
+++ WS03/Framework/Router.php
@@ -3,6 +3,7 @@
 namespace Framework;
 
 use App\Controllers\ErrorController;
+use Framework\Middleware\Authorize;
 
 // $routes = require basePath('routes.php');
 
@@ -22,10 +23,12 @@ class Router
      * @param string $method
      * @param string $uri
      * @param string $action
+     * @param array $middleware
+     * 
      * @return void
      */
 
-    public function registerRoute($method, $uri, $action)
+    public function registerRoute($method, $uri, $action, $middleware = [])
     {
         list($controller, $controllerMethod) = explode('@', $action);
 
@@ -33,63 +36,73 @@ class Router
             'method' => $method,
             'uri' => $uri,
             'controller' => $controller,
-            'controllerMethod' => $controllerMethod
+            'controllerMethod' => $controllerMethod,
+            'middleware' => $middleware
         ];
     }
 
     /**
      * Add a GET route
      * @param string $uri
-     * @param $controller
-     * return void
+     * @param string $controller
+     * @param array $middleware
+     * 
+     * @return void
      */
 
-    public function get($uri, $controller)
+    public function get($uri, $controller, $middleware = [])
     {
-        $this->registerRoute('GET', $uri, $controller);
+        $this->registerRoute('GET', $uri, $controller, $middleware);
     }
 
     /**
      * Add a POST route
      * @param string $uri
-     * @param $controller
-     * return void
+     * @param string $controller
+     * @param array $middleware
+     * 
+     * @return void
      */
 
-    public function post($uri, $controller)
+    public function post($uri, $controller, $middleware = [])
     {
-        $this->registerRoute('POST', $uri, $controller);
+        $this->registerRoute('POST', $uri, $controller, $middleware);
     }
 
     /**
      * Add a PUT route
      * @param string $uri
-     * @param $controller
-     * return void
+     * @param string $controller
+     * @param array $middleware
+     * 
+     * @return void
      */
 
-    public function put($uri, $controller)
+    public function put($uri, $controller, $middleware = [])
     {
-        $this->registerRoute('PUT', $uri, $controller);
+        $this->registerRoute('PUT', $uri, $controller, $middleware);
     }
 
     /**
      * Add a DELETE route
      * @param string $uri
-     * @param $controller
-     * return void
+     * @param string $controller
+     * @param array $middleware
+     * 
+     * @return void
      */
 
-    public function delete($uri, $controller)
+    public function delete($uri, $controller, $middleware = [])
     {
-        $this->registerRoute('DELETE', $uri, $controller);
+        $this->registerRoute('DELETE', $uri, $controller, $middleware);
     }
 
 
     /**
      * Route the request
      * @param string $uri
-     * return void
+     * 
+     * @return void
      */
 
     public function route($uri)
@@ -132,6 +145,10 @@ class Router
                 }
 
                 if ($match) {
+
+                    foreach ($route['middleware'] as $middleware) {
+                        (new Authorize())->handle($middleware);
+                    }
                     //Extract controller and controller method
                     $controller = 'App\\Controllers\\' . $route['controller'];
                     $controllerMethod = $route['controllerMethod'];
--- WS03/routes.php
+++ WS03/routes.php
@@ -2,20 +2,20 @@
 
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
-$router->get('/listings/create', 'ListingController@create');
-$router->get('/listings/edit/{id}', 'ListingController@edit');
+$router->get('/listings/create', 'ListingController@create', ['auth']);
+$router->get('/listings/edit/{id}', 'ListingController@edit', ['auth']);
 $router->get('/listings/{id}', 'ListingController@show');
-$router->get('/auth/register', 'UserController@create');
-$router->get('/auth/login', 'UserController@login');
+$router->get('/auth/register', 'UserController@create', ['guest']);
+$router->get('/auth/login', 'UserController@login', ['guest']);
 
-$router->post('/listings', 'ListingController@store');
-$router->post('/auth/register', 'UserController@store');
-$router->post('/auth/login', 'UserController@authenticate');
-$router->post('/auth/logout', 'UserController@logout');
+$router->post('/listings', 'ListingController@store', ['auth']);
+$router->post('/auth/register', 'UserController@store', ['guest']);
+$router->post('/auth/login', 'UserController@authenticate', ['guest']);
+$router->post('/auth/logout', 'UserController@logout', ['auth']);
 
-$router->put('/listings/{id}', 'ListingController@update');
+$router->put('/listings/{id}', 'ListingController@update', ['auth']);
 
-$router->delete('/listings/{id}', 'ListingController@destroy');
+$router->delete('/listings/{id}', 'ListingController@destroy', ['auth']);
 
 // return [
 //     '/' => 'controllers/home.php',
