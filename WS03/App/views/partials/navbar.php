99823a9cd3aedd539d3c451b7eb76707673857a8
Lan Angeles <lanargied.angeles2004@gmail.com>
2026-05-19 13:41:05 +0800

    Login and Authorize Middleware 1/2

--- WS03/App/views/partials/navbar.php
+++ WS03/App/views/partials/navbar.php
@@ -12,9 +12,8 @@
          </h1>
          <nav class="space-x-4">
              <?php if (Session::has('user')) : ?>
-
-                 <div>Welcome, <?= Session::get('user')['name'] ?></div>
                  <div class="flex justify-between items-center gap4">
+                     <div>Welcome, <?= Session::get('user')['name'] ?></div>
                      <form method="POST" action="/auth/logout">
                          <button type="submit" class="text-white hover:underline px-4">Log Out</button>
                      </form>
@@ -26,9 +25,7 @@
              <?php else: ?>
                  <a href="/auth/login" class="text-white hover:underline">Login</a>
                  <a href="/auth/register" class="text-white hover:underline">Register</a>
-                 <a
-                     href="/listings/create"
-                     class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
+
              <?php endif; ?>
 
          </nav>
